68301fe0d5b441e4aaa1b4c66d74481a2fc72ab5
BryanWg <wongbryan1@gmail.com>
2021-07-20 19:02:31 +0800

    hihi

--- Backend/routes/api/restaurants.js
+++ Backend/routes/api/restaurants.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const axios = require('axios')
+const { parse, stringify, toJSON, fromJSON } = require('flatted');
+
 router.get('/', (req, res) => {
     const key = 'AIzaSyA16hl91N_Ecfoa25neGfdITZqObq0DZcE'
     const neighborhood = 'chelsea'
@@ -8,9 +10,11 @@ router.get('/', (req, res) => {
     const category = 'burgers'
 
     axios.get(
-        `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${category}+${neighborhood}+${borough}+${city}&type=restaurant&key=${key}`
+        //`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${category}+${neighborhood}+${borough}+${city}&type=restaurant&key=${key}`
+        //`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${key}`
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyA16hl91N_Ecfoa25neGfdITZqObq0DZcE'
     )
-        .then(restaurants => res.send(restaurants))
+        .then(restaurants => res.send(stringify(restaurants)))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
