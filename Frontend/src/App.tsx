1a496d5c01570b99937dfd1b00b56a54cd46cb77
BryanWg <wongbryan1@gmail.com>
2021-07-20 18:49:06 +0800

    hehe

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,13 +5,19 @@ import TinderCard from 'react-tinder-card'
 import Card from './Pages/main';
 import { Box, Flex } from '@chakra-ui/react';
 import GetRestaurant from './functions';
+import axios from 'axios';
+import { stringify } from 'flatted';
 
 function App() {
   GetRestaurant().then(data => console.log(data))//.catch(error => console.log(error)); 
-   
+  axios.get(
+    'http://localhost:5000/restaurants'
+  )
+    .then(restaurants => console.log(stringify(restaurants)))
+    .catch(err => console.log('Error: ' + err));
   return (
     <Flex align="center" justifyContent="center" justifyItems="center">
-      <Box  width="90vw" maxW="500px" h="300px"> 
+      <Box width="90vw" maxW="500px" h="300px">
         <Card />
         <Card />
         <Card />
